7fff708922628343ee704c95452f0c6a783e7354
Nguyễn Tuấn <nguyentuanit96@gmail.com>
2016-11-18 13:38:36 +0700

    Initial commit

---
+++ src/ricepaper/rcp/Application.java
@@ -0,0 +1,21 @@
+package ricepaper.rcp;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * This class controls all aspects of the application's execution
+ */
+public class Application implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		System.out.println("Hello RCP World!");
+		return IApplication.EXIT_OK;
+	}
+
+	@Override
+	public void stop() {
+		// nothing to do
+	}
+}
---
+++ src/ricepaper/rcp/util/Encryption.java
@@ -0,0 +1,5 @@
+package ricepaper.rcp.util;
+
+public class Encryption {
+
+}
